77edbb90ca953acb381f92883030a9a38e66ab7a
Joaquin-Garola <joaquingarola75@gmail.com>
2022-05-09 18:25:33 -0300

    Carga inicial de archivos

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>e-commerce</h1>
+        <p>
+          in process...
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
